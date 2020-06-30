6f9dfae58931c4728bf78fb658b45317df026a34
mariajop <mariajops@gmail.com>
2020-06-30 10:15:30 +0200

    game function working

--- Módulo-2-evaluación-intermedia-mj/main.js
+++ Módulo-2-evaluación-intermedia-mj/main.js
@@ -3,29 +3,37 @@
 //CONSTANTES GENERALES:
 
 const btn = document.querySelector('.js-submit-number');
-const clue = clue.innerHtml;
+const clue = document.querySelector('.js-clue');
+const counter = document.querySelector('.js-counter');
 
 //GENERAR RANDOM NUMBER:
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(getRandomNumber(100));
+const randomNumber = getRandomNumber(100);
+
+console.log(randomNumber);
 
 //QUÉ HACER CON EL USER INPUT:
 
-function handleNumberInput() {
-  const userNumber = document.querySelector('.js-input-number').value;
+function handleNumberInput(event) {
+  event.preventDefault();
+  const userNumber = parseInt(document.querySelector('.js-input-number').value);
   console.log(userNumber);
+  console.log(randomNumber);
 
-  if (userNumber === getRandomNumber) {
+  if (userNumber === randomNumber) {
+    clue.innerHTML = '¡¡Enhorabuena, has acertado!!';
+  } else if (userNumber >= 101) {
+    clue.innerHTML = 'Recuerda que el número ha de estar entre 0 y 100 ;) ';
+  } else if (userNumber <= randomNumber) {
+    clue.innerHTML = 'Este número es demasiado bajo, ¡prueba otra vez! :) ';
+  } else if (userNumber >= randomNumber) {
+    clue.innerHTML = 'Este número es demasiado alto, ¡prueba otra vez! :) ';
   }
 }
 
 btn.addEventListener('click', handleNumberInput);
 
-//Cuando se envía input:
-//acceder a input.value y mostrar en consola
-//if, else,
-//mostrar pista o msg éxito
-//Update counter
+//UPDATE COUNTER!!!!
