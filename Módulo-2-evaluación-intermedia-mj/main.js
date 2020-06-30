6591cd24e79d5db957fe8eef601607064133637c
mariajop <mariajops@gmail.com>
2020-06-30 09:39:16 +0200

    drafting first constants and logic

--- Módulo-2-evaluación-intermedia-mj/main.js
+++ Módulo-2-evaluación-intermedia-mj/main.js
@@ -1,6 +1,31 @@
 'use strict';
 
+//CONSTANTES GENERALES:
+
+const btn = document.querySelector('.js-submit-number');
+const clue = clue.innerHtml;
+
+//GENERAR RANDOM NUMBER:
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(getRandomNumber);
+console.log(getRandomNumber(100));
+
+//QUÉ HACER CON EL USER INPUT:
+
+function handleNumberInput() {
+  const userNumber = document.querySelector('.js-input-number').value;
+  console.log(userNumber);
+
+  if (userNumber === getRandomNumber) {
+  }
+}
+
+btn.addEventListener('click', handleNumberInput);
+
+//Cuando se envía input:
+//acceder a input.value y mostrar en consola
+//if, else,
+//mostrar pista o msg éxito
+//Update counter
